3d8b0be7fc296b122681abde61d4fbce09b7e7e9
darkkennn <sacpan2000@gmail.com>
2025-08-30 12:43:27 +0530

    creating requirements.txt and data.py

---
+++ data.py
@@ -0,0 +1,32 @@
+moored_buoy_locations = {
+    "AD06": {"coords": [18.5, 67], "color": "darkblue", "icon": "anchor"},
+    "AD07": {"coords": [15, 68], "color": "darkblue", "icon": "anchor"},
+    "AD08": {"coords": [12, 68], "color": "darkblue", "icon": "anchor"},
+    "AD10": {"coords": [10.3, 72.58], "color": "darkblue", "icon": "anchor"},
+    "CALVAL": {"coords": [10.5, 72.1], "color": "red", "icon": "anchor"},
+    "CB02": {"coords": [10.89, 72.2], "color": "red", "icon": "anchor"},
+    "CAU19": {"coords": [9, 72], "color": "red", "icon": "anchor"},
+    "CB01": {"coords": [9, 90], "color": "red", "icon": "anchor"},
+    "BD10": {"coords": [16, 87], "color": "green", "icon": "anchor"},
+    "BD11": {"coords": [13.5, 84], "color": "green", "icon": "anchor"},
+    "BD13": {"coords": [14, 86.5], "color": "green", "icon": "anchor"},
+    "BD12": {"coords": [10.7, 93.7], "color": "green", "icon": "anchor"},
+    "BD14": {"coords": [6.5, 88.3], "color": "green", "icon": "anchor"},
+}
+
+AWS_locations = {
+    "AWS1": {"coords": [9.96194, 76.28306], "color": "blue", "icon": "cloud"},
+    "AWS2": {"coords": [11.04028, 74.97611], "color": "blue", "icon": "cloud"},
+    "AWS3": {"coords": [22.54056, 88.3025], "color": "blue", "icon": "cloud"},
+}
+
+Drifting_buoy_locations = {
+    "I300534064138620": {"coords": [1.2659416, 94.2979406], "color": "orange", "icon": "life-ring"},
+    "I300534064139130": {"coords": [-59.6781261, 34.2363898], "color": "orange", "icon": "life-ring"},
+    "IN0017": {"coords": [4.885, 72.9357], "color": "orange", "icon": "life-ring"},
+    "AR020220830": {"coords": [6.3439, 80.0438], "color": "orange", "icon": "life-ring"},
+    "IN0038": {"coords": [15.4683, -93.3828], "color": "orange", "icon": "life-ring"},
+    "IN0035": {"coords": [16.9798, 69.4158], "color": "orange", "icon": "life-ring"},
+    "IN0030": {"coords": [15.9896, 72.5854], "color": "orange", "icon": "life-ring"},
+    "I300534064138600": {"coords": [-12.0335904, 987.2484693], "color": "orange", "icon": "life-ring"},
+}
